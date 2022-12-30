d8ab4e0d0f2e40593417daf4f254956dd77eb5fd
Ömer Faruk İngeç <49835381+omer1325@users.noreply.github.com>
2022-12-30 04:51:10 +0300

    Added New API
    - Added Order API
    
    - Used CQRS and DDD

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -19,6 +19,7 @@ namespace FreeCourse.IdentityServer
             new ApiResource("resource_photo_stock"){Scopes = {"photo_stock_fullpermission"}},
             new ApiResource("resource_basket"){Scopes = {"basket_fullpermission"}},
             new ApiResource("resource_discount"){Scopes = {"discount_fullpermission"}},
+            new ApiResource("resource_order"){Scopes = {"order_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -39,6 +40,7 @@ namespace FreeCourse.IdentityServer
                 new ApiScope("photo_stock_fullpermission", "Full access for Photo Stock API"),
                 new ApiScope("basket_fullpermission", "Full access for Basket API"),
                 new ApiScope("discount_fullpermission", "Full access for Discount API"),
+                new ApiScope("order_fullpermission", "Full access for Order API"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -68,6 +70,7 @@ namespace FreeCourse.IdentityServer
                     {
                         "basket_fullpermission",
                         "discount_fullpermission",
+                        "order_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
